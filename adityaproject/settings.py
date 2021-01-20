c83174d7505d8cf7402a4dd49d00109533e30ff1
Bharat Tiwari <bharattiwari9926@gmail.com>
2021-01-20 21:17:27 +0530

    settings changed

--- adityaproject/settings.py
+++ adityaproject/settings.py
@@ -110,11 +110,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 django_heroku.settings(locals())
