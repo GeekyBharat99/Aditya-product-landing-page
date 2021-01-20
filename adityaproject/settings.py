e4747cf867351759ce631b425531a9c88f510df4
Bharat Tiwari <bharattiwari9926@gmail.com>
2021-01-20 21:27:21 +0530

    changed

--- adityaproject/settings.py
+++ adityaproject/settings.py
@@ -110,10 +110,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 django_heroku.settings(locals())
